3127118e1a2fe945f91fb31130f12c28cc499f38
omes773 <olockhart88@gmail.com>
2023-09-28 23:32:45 -0500

    local storage

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ function renderTime() {
 renderTime();
 setInterval(renderTime, 1000);
 
+var time = ["hour-9","hour-10","hour-11","hour-12","hour-13","hour-14","hour-15", "hour-16", "hour-17"]
 
+var saveButton = $("<button>");
+saveButton.text("save");
+
+saveButton.on("click", function() {
+  $(".past").append(saveButton);
+
+  var time = localStorage.getItem("time") ? 
+JSON.parse(localStorage.getItem("time")) : [];
+
+for (var i = 0; i < time.length; i++) {
+  
+  
+}
+});
